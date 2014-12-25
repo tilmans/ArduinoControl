d6aead2c0e6a0b1b6d4d6a776677d1b69fe83ece
Tilman <tilman@tilmans-mbp.speedport_w_724v_typ_a_05011602_00_001>
2014-12-25 21:16:24 +0100

    Initial Commit

---
+++ ArduinoConnect/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ArduinoConnect
+//
+//  Created by Tilman on 25/12/14.
+//  Copyright (c) 2014 Tilman. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
